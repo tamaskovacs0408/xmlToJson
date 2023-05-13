3ce396a800d4d75dff7bfa731a36f2b4fb8bdafd
Tamás Kovács <kovacs.tamas0408@gmail.com>
2023-05-13 21:40:35 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Xml;
+
+namespace XMLtoJSONConverter
+{
+  class Program
+  {
+    public static void Main(string[] args)
+    {
+      string xmlFilePath = "./inputs/sample.xml";
+      string jsonFilePath = "./outputs/sample.json";
+      // Reading XML file
+      string xml = File.ReadAllText(xmlFilePath);
+      // Processing XML and crearing JSON file
+      XmlDocument xmlDocument = new XmlDocument();
+      xmlDocument.LoadXml(xml);
+
+      // Get the book elements from the XML and convert them to the json file
+      var bookElements = xmlDocument.SelectNodes("//book");
+      JArray booksArray = new JArray();
+
+      foreach (XmlNode bookNode in bookElements)
+      {
+        string bookJson = JsonConvert.SerializeXmlNode(bookNode);
+        JObject bookObject = JObject.Parse(bookJson);
+        booksArray.Add(bookObject);
+      }
+
+      // Saving json file
+      File.WriteAllTextAsync(jsonFilePath, booksArray.ToString());
+
+      Console.WriteLine("Converting successfully finished!");
+    }
+  }
+}
